62acd3f2c40a54294297e3c0337ea13525b2fb5c
ReimuNotMoe <34613827+ReimuNotMoe@users.noreply.github.com>
2021-05-23 01:48:13 +0800

    Enable GCP config bit for all boards by default so only the BL can do readback

--- Boards/CartBoy_RW_v2/PICoBoot_Board.c
+++ Boards/CartBoy_RW_v2/PICoBoot_Board.c
@@ -27,26 +27,26 @@
 #pragma config WPEND = WPENDMEM    //Segment Write Protection End Page Select bit->Write Protect from WPFP to the last page of memory
 
 // CONFIG2
-#pragma config POSCMOD = XT    //Primary Oscillator Select->XT oscillator mode selected
-#pragma config DISUVREG = OFF    //Internal USB 3.3V Regulator Disable bit->Regulator is disabled
-#pragma config IOL1WAY = OFF    //IOLOCK One-Way Set Enable bit->Write RP Registers Once
-#pragma config OSCIOFNC = ON    //Primary Oscillator Output Function->OSCO functions as port I/O (RC15)
-#pragma config FCKSM = CSECMD    //Clock Switching and Monitor->Clock switching is enabled, Fail-safe Clock Monitor is disabled
-#pragma config FNOSC = FRC    //Oscillator Select->FRC
-#pragma config PLL_96MHZ = ON    //96MHz PLL Disable->Enabled
-#pragma config PLLDIV = DIV2    //USB 96 MHz PLL Prescaler Select bits->Oscillator input divided by 2 (8MHz input)
-#pragma config IESO = OFF    //Internal External Switch Over Mode->IESO mode (Two-speed start-up)disabled
+#pragma config POSCMOD = XT		// Primary Oscillator Select->XT oscillator mode selected
+#pragma config DISUVREG = OFF		// Internal USB 3.3V Regulator Disable bit->Regulator is disabled
+#pragma config IOL1WAY = OFF		// IOLOCK One-Way Set Enable bit->Write RP Registers Once
+#pragma config OSCIOFNC = ON		// Primary Oscillator Output Function->OSCO functions as port I/O (RC15)
+#pragma config FCKSM = CSECMD		// Clock Switching and Monitor->Clock switching is enabled, Fail-safe Clock Monitor is disabled
+#pragma config FNOSC = FRC		// Oscillator Select->FRC
+#pragma config PLL_96MHZ = ON		// 96MHz PLL Disable->Enabled
+#pragma config PLLDIV = DIV2		// USB 96 MHz PLL Prescaler Select bits->Oscillator input divided by 2 (8MHz input)
+#pragma config IESO = OFF		// Internal External Switch Over Mode->IESO mode (Two-speed start-up)disabled
 
 // CONFIG1
-#pragma config WDTPS = PS32768    //Watchdog Timer Postscaler->1:32768
-#pragma config FWPSA = PR128    //WDT Prescaler->Prescaler ratio of 1:128
-#pragma config WINDIS = OFF    //Watchdog Timer Window->Standard Watchdog Timer enabled,(Windowed-mode is disabled)
-#pragma config FWDTEN = OFF    //Watchdog Timer Enable->Watchdog Timer is disabled
-#pragma config ICS = PGx2    //Comm Channel Select->Emulator functions are shared with PGEC1/PGED1
-#pragma config BKBUG = OFF    //Background Debug->Device resets into Operational mode
-#pragma config GWRP = OFF    //General Code Segment Write Protect->Writes to program memory are allowed
-#pragma config GCP = OFF    //General Code Segment Code Protect->Code protection is disabled
-#pragma config JTAGEN = OFF    //JTAG Port Enable->JTAG port is disabled
+#pragma config WDTPS = PS32768		// Watchdog Timer Postscaler->1:32768
+#pragma config FWPSA = PR128		// WDT Prescaler->Prescaler ratio of 1:128
+#pragma config WINDIS = OFF		// Watchdog Timer Window->Standard Watchdog Timer enabled,(Windowed-mode is disabled)
+#pragma config FWDTEN = OFF		// Watchdog Timer Enable->Watchdog Timer is disabled
+#pragma config ICS = PGx2		// Comm Channel Select->Emulator functions are shared with PGEC1/PGED1
+#pragma config BKBUG = OFF		// Background Debug->Device resets into Operational mode
+#pragma config GWRP = OFF		// General Code Segment Write Protect->Writes to program memory are allowed
+#pragma config GCP = ON			// General Code Segment Code Protect->Code protection is **enabled**
+#pragma config JTAGEN = OFF		// JTAG Port Enable->JTAG port is disabled
 
 const uint32_t XTAL_FREQ = 32000000UL;
 const uint32_t FCY = 32000000UL / 2;
--- Boards/PotatoPi_PICo24/PICoBoot_Board.c
+++ Boards/PotatoPi_PICo24/PICoBoot_Board.c
@@ -21,33 +21,33 @@
 #include <PICoBoot/PICoBoot.h>
 
 // CONFIG3
-#pragma config WPFP = WPFP255    //Write Protection Flash Page Segment Boundary->Highest Page (same as page 170)
-#pragma config SOSCSEL = EC    //Secondary Oscillator Power Mode Select->External clock (SCLKI) or Digital I/O mode(
-#pragma config WUTSEL = LEG    //Voltage Regulator Wake-up Time Select->Default regulator start-up time is used
-#pragma config ALTPMP = ALPMPDIS    //Alternate PMP Pin Mapping->EPMP pins are in default location mode
-#pragma config WPDIS = WPDIS    //Segment Write Protection Disable->Segmented code protection is disabled
-#pragma config WPCFG = WPCFGDIS    //Write Protect Configuration Page Select->Last page (at the top of program memory) and Flash Configuration Words are not write-protected
-#pragma config WPEND = WPENDMEM    //Segment Write Protection End Page Select->Protected code segment upper boundary is at the last page of program memory; the lower boundary is the code page specified by WPFP
+#pragma config WPFP = WPFP255		// Write Protection Flash Page Segment Boundary->Highest Page (same as page 170)
+#pragma config SOSCSEL = EC		// Secondary Oscillator Power Mode Select->External clock (SCLKI) or Digital I/O mode(
+#pragma config WUTSEL = LEG		// Voltage Regulator Wake-up Time Select->Default regulator start-up time is used
+#pragma config ALTPMP = ALPMPDIS	// Alternate PMP Pin Mapping->EPMP pins are in default location mode
+#pragma config WPDIS = WPDIS		// Segment Write Protection Disable->Segmented code protection is disabled
+#pragma config WPCFG = WPCFGDIS		// Write Protect Configuration Page Select->Last page (at the top of program memory) and Flash Configuration Words are not write-protected
+#pragma config WPEND = WPENDMEM		// Segment Write Protection End Page Select->Protected code segment upper boundary is at the last page of program memory; the lower boundary is the code page specified by WPFP
 
 // CONFIG2
-#pragma config POSCMOD = XT    //Primary Oscillator Select->XT Oscillator mode is selected
-#pragma config IOL1WAY = ON    //IOLOCK One-Way Set Enable->The IOLOCK bit (OSCCON<6>) can be set once, provided the unlock sequence has been completed. Once set, the Peripheral Pin Select registers cannot be written to a second time.
-#pragma config OSCIOFNC = ON    //OSCO Pin Configuration->OSCO/CLKO/RC15 functions as port I/O (RC15)
-#pragma config FCKSM = CSECMD    //Clock Switching and Fail-Safe Clock Monitor->Clock switching is enabled, Fail-Safe Clock Monitor is disabled
-#pragma config FNOSC = FRC    //Initial Oscillator Select->FRC
-#pragma config PLL96MHZ = ON    //96MHz PLL Startup Select->96 MHz PLL is enabled automatically on start-up
-#pragma config PLLDIV = DIV2    //96 MHz PLL Prescaler Select->Oscillator input is divided by 2 (8 MHz input)
-#pragma config IESO = OFF    //Internal External Switchover->IESO mode (Two-Speed Start-up) is disabled
+#pragma config POSCMOD = XT		// Primary Oscillator Select->XT Oscillator mode is selected
+#pragma config IOL1WAY = ON		// IOLOCK One-Way Set Enable->The IOLOCK bit (OSCCON<6>) can be set once, provided the unlock sequence has been completed. Once set, the Peripheral Pin Select registers cannot be written to a second time.
+#pragma config OSCIOFNC = ON		// OSCO Pin Configuration->OSCO/CLKO/RC15 functions as port I/O (RC15)
+#pragma config FCKSM = CSECMD		// Clock Switching and Fail-Safe Clock Monitor->Clock switching is enabled, Fail-Safe Clock Monitor is disabled
+#pragma config FNOSC = FRC		// Initial Oscillator Select->FRC
+#pragma config PLL96MHZ = ON		// 96MHz PLL Startup Select->96 MHz PLL is enabled automatically on start-up
+#pragma config PLLDIV = DIV2		// 96 MHz PLL Prescaler Select->Oscillator input is divided by 2 (8 MHz input)
+#pragma config IESO = OFF		// Internal External Switchover->IESO mode (Two-Speed Start-up) is disabled
 
 // CONFIG1
-#pragma config WDTPS = PS32768    //Watchdog Timer Postscaler->1:32768
-#pragma config FWPSA = PR128    //WDT Prescaler->Prescaler ratio of 1:128
-#pragma config WINDIS = OFF    //Windowed WDT->Standard Watchdog Timer enabled,(Windowed-mode is disabled)
-#pragma config FWDTEN = OFF    //Watchdog Timer->Watchdog Timer is disabled
-#pragma config ICS = PGx3    //Emulator Pin Placement Select bits->Emulator functions are shared with PGEC3/PGED3
-#pragma config GWRP = OFF    //General Segment Write Protect->Writes to program memory are allowed
-#pragma config GCP = OFF    //General Segment Code Protect->Code protection is disabled
-#pragma config JTAGEN = OFF    //JTAG Port Enable->JTAG port is disabled
+#pragma config WDTPS = PS32768		// Watchdog Timer Postscaler->1:32768
+#pragma config FWPSA = PR128		// WDT Prescaler->Prescaler ratio of 1:128
+#pragma config WINDIS = OFF		// Windowed WDT->Standard Watchdog Timer enabled,(Windowed-mode is disabled)
+#pragma config FWDTEN = OFF		// Watchdog Timer->Watchdog Timer is disabled
+#pragma config ICS = PGx3		// Emulator Pin Placement Select bits->Emulator functions are shared with PGEC3/PGED3
+#pragma config GWRP = OFF		// General Segment Write Protect->Writes to program memory are allowed
+#pragma config GCP = ON			// General Segment Code Protect->Code protection is **enabled**
+#pragma config JTAGEN = OFF		// JTAG Port Enable->JTAG port is disabled
 
 const uint32_t XTAL_FREQ = 32000000UL;
 const uint32_t FCY = 32000000UL / 2;
